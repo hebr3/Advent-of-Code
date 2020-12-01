c3e08f158040f117bc4cb37e32d98454ec30ae08
hebr3 <brook057@cougars.csusm.edu>
2020-11-30 21:53:09 -0800

    First day done

--- 2020/Day01/Day01.rkt
+++ 2020/Day01/Day01.rkt
@@ -1,7 +1,41 @@
 #lang racket
-;test
-
 (require threading)
 
-(define input
-  (file->lines "input.txt"))
+(define data
+  (~> "input.txt"
+      open-input-file
+      (read-line _ 'return)
+      (string-split _ "\n")
+      (map string->number _)))
+
+(define (find-2020-pair list-of-numbers)
+  (define (iter head tail)
+    (for/or ([i tail])
+      (if (zero? (- 2020 head i))
+          (* head i)
+          #f)))
+  (or (iter (first list-of-numbers) (rest list-of-numbers))
+      (find-2020-pair (rest list-of-numbers))))
+
+(define (find-2020-triple list-of-numbers)
+  (define (iter2 one two rst)
+    (for/or ([i rst])
+      (if (zero? (- 2020 one two i))
+          (* one two i)
+          #f)))
+  (define (iter head tail)
+    (for/or ([i (range (length tail))])
+      (iter2 head (list-ref tail i) (drop tail i))))
+  (or (iter (first list-of-numbers)
+            (rest list-of-numbers))
+      (find-2020-triple (rest list-of-numbers))))
+
+(display "one: ")
+(displayln 
+ (~> data
+     find-2020-pair))
+
+(display "two: ")
+(displayln
+ (~> data
+     find-2020-triple))
