0b584f06d0c691b41b1dacbba87d78f791460fd7
hebr3 <brook057@cougars.csusm.edu>
2020-12-01 21:44:26 -0800

    Need to clean these up later

--- 2020/Day02/Day02.rkt
+++ 2020/Day02/Day02.rkt
@@ -1,6 +1,50 @@
 #lang racket
-
 (require threading)
+(require racket/match)
 
-(define input
-  (file->lines "input.txt"))
+(define test
+  "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc")
+
+(define data
+  (~> "input.txt"
+      open-input-file
+      (read-line _ 'return-linefeed)))
+
+(define (valid-password? str)
+  (match-let ([(list a b c d e) (string-split str #px"\\-|\\ |:")])
+    (<= (string->number a)
+        (count (λ (x) (char=? (first (string->list c)) x)) (string->list e))
+        (string->number b))))
+
+(define (valid-password-2? str)
+  (match-let ([(list a b c d e) (string-split str #px"\\-|\\ |:")])
+    (xor (char=? (first (string->list c))
+                 (list-ref (string->list e) (sub1 (string->number a))))
+         (char=? (first (string->list c))
+                 (list-ref (string->list e) (sub1 (string->number b)))))))
+
+(display "test 1: ")
+(~> test
+    (string-split _ "\n")
+    (map valid-password? _)
+    (count identity _))
+
+(display "one: ")
+(~> data
+    (string-split _ "\n")
+    (map valid-password? _)
+    (count identity _))
+
+(display "test 2: ")
+(~> test
+    (string-split _ "\n")
+    (map valid-password-2? _)
+    (count identity _))
+
+(display "two: ")
+(~> data
+    (string-split _ "\n")
+    (map valid-password-2? _)
+    (count identity _))
