eb2809eef74ab395a36f2225d19f2350f3621c8b
henry brooks <brook057@cougars.csusm.edu>
2021-12-01 21:44:39 -0800

    Update 2021-01.rkt

--- 2021-01.rkt
+++ 2021-01.rkt
@@ -28,29 +28,3 @@
 
 (count-increases-three test)
 (count-increases-three data)
-
-;(define (find-target-pair target)
-;  (λ (list-of-numbers)
-;    (for/or ([i list-of-numbers])
-;      (let ([j (- target i)])
-;        (and (member j list-of-numbers)
-;             (* i j))))))
-;
-;(define find-2020-pair (find-target-pair 2020))
-;
-;(define (find-2020-triple list-of-numbers)
-;  (define i (first list-of-numbers))
-;  (define find-2020-minus-first (find-target-pair (- 2020 i)))
-;  (or (let ([jk (find-2020-minus-first (rest list-of-numbers))])
-;        (and jk (* i jk)))
-;      (find-2020-triple (rest list-of-numbers))))
-;
-;(display "one: ")
-;(displayln 
-; (~> data
-;     find-2020-pair))
-;
-;(display "two: ")
-;(displayln
-; (~> data
-;     find-2020-triple))
