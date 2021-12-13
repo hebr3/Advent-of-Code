7bb74897e2a4bde9ba6697a8177404a9fc918950
hebr3 <brook057@cougars.csusm.edu>
2021-12-13 12:43:08 -0800

    Not sure if faster, but I think it's cleaner this way

--- 2021-06.rkt
+++ 2021-06.rkt
@@ -11,72 +11,48 @@
 (define test
   '("3,4,3,1,2"))
 
-(define identity (λ (x) x))
-
-(define (bundle-list L)
-  (vector (car L) (length L)))
+(define (initialize LoN)
+  (for/list ([i 9]) (cons i (count (λ (n) (= n i)) LoN))))
 
 (define (format-data str)
   (~> str
       car
       (string-split _ ",")
       (map string->number _)
-      (group-by identity _)
-      (map bundle-list _)))
+      initialize))
 
-(define (flatten-fish-set LoF)
-  (define HASH (make-hash))
-  (for ([f (flatten LoF)])
-    (match-let ([(vector age cnt) f])
-      (let ([fish-cnt (hash-ref HASH age #f)])
-      (if fish-cnt
-          (hash-set! HASH age (+ cnt fish-cnt))
-          (hash-set! HASH age cnt)))))
-  (~> HASH
-      hash->list
-      (map (λ (x) (vector (car x) (cdr x))) _)))
-
-(define (age-fish-set fish-set)
-  (match-let ([(vector age cnt) fish-set])
-    (if (zero? age)
-        (list (vector 6 cnt) (vector 8 cnt))
-        (list (vector (sub1 age) cnt)))))
-
-(define (age-all-fish-N-days LoF N)
-  (if (zero? N)
-      LoF
-      (~> LoF
-          (map age-fish-set _)
-          flatten-fish-set
-          (age-all-fish-N-days _ (sub1 N)))))
-
-(define (sum L)
-  (apply + L))
-         
-(~> test
-    format-data
-    (age-all-fish-N-days _ 80)
-    (map (λ (x) (vector-ref x 1)) _)
-    sum)
+(define (update-list L)
+  (for/list ([i 9])
+    (cond
+      [(= i 6)
+       (cons i (+ (cdr (list-ref L 0))
+                  (cdr (list-ref L 7))))]
+      [(= i 8)
+       (cons i (cdr (list-ref L 0)))]
+      [else
+       (cons i (cdr (list-ref L (add1 i))))])))
     
+(define (update-N-times L N)
+  (if (zero? N)
+      L
+      (update-N-times (update-list L) (sub1 N))))
 
-(~> data
-    format-data
-    (age-all-fish-N-days _ 80)
-    ;length
-    (map (λ (x) (vector-ref x 1)) _)
-    sum)
+(for/sum ([i (~> test
+                 format-data
+                 (update-N-times _ 80))])
+  (cdr i))
+    
+(for/sum ([i (~> data
+                 format-data
+                 (update-N-times _ 80))])
+  (cdr i))
 
-(~> test
-    format-data
-    (age-all-fish-N-days _ 256)
-    ;length
-    (map (λ (x) (vector-ref x 1)) _)
-    sum)
+(for/sum ([i (~> test
+                 format-data
+                 (update-N-times _ 256))])
+  (cdr i))
 
-(~> data
-    format-data
-    (age-all-fish-N-days _ 256)
-    ;length
-    (map (λ (x) (vector-ref x 1)) _)
-    sum)
+(for/sum ([i (~> data
+                 format-data
+                 (update-N-times _ 256))])
+  (cdr i))
