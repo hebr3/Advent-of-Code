f65514ece6a69126f988fa71215aca5af7c22c36
hebr3 <brook057@cougars.csusm.edu>
2021-12-13 12:20:05 -0800

    Improved the code by calculating on sets of fish

--- 2021-06.rkt
+++ 2021-06.rkt
@@ -11,31 +11,72 @@
 (define test
   '("3,4,3,1,2"))
 
+(define identity (λ (x) x))
+
+(define (bundle-list L)
+  (vector (car L) (length L)))
+
 (define (format-data str)
   (~> str
       car
       (string-split _ ",")
-      (map string->number _)))
+      (map string->number _)
+      (group-by identity _)
+      (map bundle-list _)))
 
-(define (age-fish num)
-  (if (zero? num)
-      (list 6 8)
-      (list (sub1 num))))
+(define (flatten-fish-set LoF)
+  (define HASH (make-hash))
+  (for ([f (flatten LoF)])
+    (match-let ([(vector age cnt) f])
+      (let ([fish-cnt (hash-ref HASH age #f)])
+      (if fish-cnt
+          (hash-set! HASH age (+ cnt fish-cnt))
+          (hash-set! HASH age cnt)))))
+  (~> HASH
+      hash->list
+      (map (λ (x) (vector (car x) (cdr x))) _)))
+
+(define (age-fish-set fish-set)
+  (match-let ([(vector age cnt) fish-set])
+    (if (zero? age)
+        (list (vector 6 cnt) (vector 8 cnt))
+        (list (vector (sub1 age) cnt)))))
 
 (define (age-all-fish-N-days LoF N)
   (if (zero? N)
       LoF
       (~> LoF
-          (map age-fish _)
-          flatten
+          (map age-fish-set _)
+          flatten-fish-set
           (age-all-fish-N-days _ (sub1 N)))))
 
+(define (sum L)
+  (apply + L))
+         
 (~> test
     format-data
     (age-all-fish-N-days _ 80)
-    length)
+    (map (λ (x) (vector-ref x 1)) _)
+    sum)
+    
 
 (~> data
     format-data
     (age-all-fish-N-days _ 80)
-    length)
+    ;length
+    (map (λ (x) (vector-ref x 1)) _)
+    sum)
+
+(~> test
+    format-data
+    (age-all-fish-N-days _ 256)
+    ;length
+    (map (λ (x) (vector-ref x 1)) _)
+    sum)
+
+(~> data
+    format-data
+    (age-all-fish-N-days _ 256)
+    ;length
+    (map (λ (x) (vector-ref x 1)) _)
+    sum)
