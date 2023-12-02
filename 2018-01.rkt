0b087e3c94821c097f5775021c5aea5c13cd1217
henry brooks <brook057@cougars.csusm.edu>
2023-12-02 09:51:17 -0800

    Update 2018-01.rkt
    Back fill

--- 2018-01.rkt
+++ 2018-01.rkt
@@ -1,31 +1,82 @@
 #lang racket
 (require threading)
 
-(define test '(1 1 3 3 4 -2 -4 3 3 4 -2 -4 1 1 1))
+(define (input->data filename)
+  (~> filename
+      (open-input-file #:mode 'text)
+      (read-line 'return-linefeed)
+      ))
 
-(define data
-  (~> "input.txt"
-      open-input-file
-      (read-line _ 'return)
-      (string-split _ "\n")
-      (map string->number _)))
+(define data (input->data "input/2018-01.txt"))
 
-(display "test: ")
-(displayln
- (~> test))
+(define test1 "+1
+-2
++3
++1")
+(define test2 "+1
++1
++1")
+(define test3 "+1
++1
+-2")
+(define test4 "-1
+-2
+-3")
 
-(display "one: ")
-(displayln
- (~> data
-     (apply + _)))
+;;
 
-(display "two: ")
-(displayln
- (~> data
-     ))
+(define (part-A L)
+  (define changes (string-split L "\n"))
+  (~>> changes
+       (map string->number)
+       (apply +)
+       ))
 
+(part-A test1)
+(part-A test2)
+(part-A test3)
+(part-A test4)
+(part-A data)
 
-(define (add-to-each n l)
-  (cond
-    [(empty? l) '()]
-    [else (cons (+ (first l) n) (add-to-each n (rest l)))]))
+;;
+
+(define test5 "+1
+-1")
+(define test6 "+3
++3
++4
+-2
+-4")
+(define test7 "-6
++3
++4
+-2
+-4")
+(define test8 "+7
++7
+-2
+-7
+-4")
+
+(define (part-B L)
+  (define changes (map string->number (string-split L "\n")))
+  (define LOOKUP (make-hash))
+  (define count 0)
+  (hash-set! LOOKUP 0 #t)
+  (define (iter sum i)
+    (define sum* (+ sum (list-ref changes i)))
+    (set! count (+ 1 count))
+    (cond
+      [(< 1000000 count) count]
+      [(hash-has-key? LOOKUP sum*) (list sum* count)]
+      [else
+       (hash-set! LOOKUP sum* #t)
+       (iter sum* (modulo (+ 1 i) (length changes)))]))
+  (iter 0 0))
+
+(part-B test5)
+(part-B test6)
+(part-B test7)
+(part-B test8)
+(part-B data)
+    
