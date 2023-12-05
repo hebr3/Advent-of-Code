749c652304a359ca4a94b4bd77490033c2c39315
henry brooks <brook057@cougars.csusm.edu>
2023-12-04 22:00:53 -0800

    Update 2023-05.rkt
    Works for first part but the list it builds is too big for second part

--- 2023-05.rkt
+++ 2023-05.rkt
@@ -57,80 +57,68 @@ humidity-to-location map:
          [temperature-to-humidity (rest (string-split (seventh lines) "\n"))]
          [humidity-to-location (rest (string-split (eighth lines) "\n"))])
 
-    (define (hash-ref* hs idx)
-      (if (hash-has-key? hs idx)
-          (hash-ref hs idx)
+    (define (lookup idx LoN)
+      (or (for/or ([l LoN])
+            (match-let ([(list dest src rng) (map string->number (string-split l))])
+              (if (<= src idx (+ src rng))
+                  (+ idx (- dest src))
+                  #f)))
           idx))
-    
-    (define seed-to-soil-hash (make-hash))
-    (for ([m seed-to-soil])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! seed-to-soil-hash (+ src i) (+ dest i)))))
-    (println 'one)
-    
-    (define soil-to-fertilizer-hash (make-hash))
-    (for ([m soil-to-fertilizer])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! soil-to-fertilizer-hash (+ src i) (+ dest i)))))
-    (println 'two)
-    
-    (define fertilizer-to-water-hash (make-hash))
-    (for ([m fertilizer-to-water])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! fertilizer-to-water-hash (+ src i) (+ dest i)))))
-    (println 'three)
-    
-    (define water-to-light-hash (make-hash))
-    (for ([m water-to-light])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! water-to-light-hash (+ src i) (+ dest i)))))
-    (println 'four)
-    
-    (define light-to-temperature-hash (make-hash))
-    (for ([m light-to-temperature])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! light-to-temperature-hash (+ src i) (+ dest i)))))
-    (println 'five)
-    
-    (define temperature-to-humidity-hash (make-hash))
-    (for ([m temperature-to-humidity])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! temperature-to-humidity-hash (+ src i) (+ dest i)))))
-    (println 'six)
-    
-    (define humidity-to-location-hash (make-hash))
-    (for ([m humidity-to-location])
-      (match-let ([(list dest src rng) (map string->number (string-split m))])
-        (for ([i rng])
-          (hash-set! humidity-to-location-hash (+ src i) (+ dest i)))))
-    (println 'seven)
-    
     (apply min
-     (for/list ([s seeds])
-       (~> s
-           (hash-ref* seed-to-soil-hash _)
-           (hash-ref* soil-to-fertilizer-hash _)
-           (hash-ref* fertilizer-to-water-hash _)
-           (hash-ref* water-to-light-hash _)
-           (hash-ref* light-to-temperature-hash _)
-           (hash-ref* temperature-to-humidity-hash _)
-           (hash-ref* humidity-to-location-hash _)
-           ))
-     )))
+           (for/list ([s seeds])
+             (~> s
+                 (lookup seed-to-soil)
+                 (lookup soil-to-fertilizer)
+                 (lookup fertilizer-to-water)
+                 (lookup water-to-light)
+                 (lookup light-to-temperature)
+                 (lookup temperature-to-humidity)
+                 (lookup humidity-to-location)
+                 ))
+           )))
 
 (part-A test)
-;(part-A data)
+(part-A data)
 
 ;;
 
-(define (part-B L)
-  L)
+(define (seed-ranges L)
+  (define (iter L* acc)
+    (if (empty? L*)
+        (flatten acc)
+        (iter (rest (rest L*)) (append (range (first L*) (+ (first L*) (second L*))) acc))))
+  (iter L '()))
 
-;(part-B test)
+(define (part-B L)
+    (let* ([lines (string-split L "\n\n")]
+         [seeds (seed-ranges (map string->number (rest (string-split (first lines)))))]
+         [seed-to-soil (rest (string-split (second lines) "\n"))]
+         [soil-to-fertilizer (rest (string-split (third lines) "\n"))]
+         [fertilizer-to-water (rest (string-split (fourth lines) "\n"))]
+         [water-to-light (rest (string-split (fifth lines) "\n"))]
+         [light-to-temperature (rest (string-split (sixth lines) "\n"))]
+         [temperature-to-humidity (rest (string-split (seventh lines) "\n"))]
+         [humidity-to-location (rest (string-split (eighth lines) "\n"))])
+
+    (define (lookup idx LoN)
+      (or (for/or ([l LoN])
+            (match-let ([(list dest src rng) (map string->number (string-split l))])
+              (if (<= src idx (+ src rng))
+                  (+ idx (- dest src))
+                  #f)))
+          idx))
+    (apply min
+           (for/list ([s seeds])
+             (~> s
+                 (lookup seed-to-soil)
+                 (lookup soil-to-fertilizer)
+                 (lookup fertilizer-to-water)
+                 (lookup water-to-light)
+                 (lookup light-to-temperature)
+                 (lookup temperature-to-humidity)
+                 (lookup humidity-to-location)
+                 ))
+           )))
+
+(part-B test)
 ;(part-B data)
