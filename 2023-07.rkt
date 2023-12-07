65348a5abe0dc6bc2417c35feeb04d82343bccaa
henry brooks <brook057@cougars.csusm.edu>
2023-12-07 09:34:19 -0800

    Create 2023-07.rkt
    Still need part 2

---
+++ 2023-07.rkt
@@ -0,0 +1,69 @@
+#lang racket
+(require threading)
+(require racket/match)
+(require "util.rkt")
+
+(define data (input->data "input/2023-07.txt"))
+
+(define test "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483")
+
+;;
+
+(struct hand [cards grp bid] #:transparent)
+
+(define (card>? a b)
+  (define card-order (string->list "AKQJT98765432"))
+  (define (iter A B)
+    (if (char=? (first A) (first B))
+        (iter (rest A) (rest B))
+        (< (index-of card-order (first A) char=?)
+           (index-of card-order (first B) char=?))))
+  (iter (string->list a) (string->list b)))
+
+(define id (λ (x) x))
+
+(define (list>? a b)
+  (> (length a) (length b)))
+
+(define (string->grp str)
+  (~>> str
+       string->list
+       (group-by id)
+       (sort _ list>?)))
+
+(define (string->hand str)
+  (match-let ([(list c b) (string-split str)])
+    (hand c (string->grp c) (string->number b))))
+
+(define (hand>? a b)
+  (match-let* ([(hand ca ga ba) a]
+               [ga-count (map length ga)]
+               [(hand cb gb bb) b]
+               [gb-count (map length gb)])
+    (cond
+      [(> (first ga-count) (first gb-count)) #t]
+      [(< (first ga-count) (first gb-count)) #f]
+      [(and (= (first ga-count) (first gb-count))
+            (> (second ga-count) (second gb-count))) #t]
+      [(and (= (first ga-count) (first gb-count))
+            (< (second ga-count) (second gb-count))) #f]
+      [else (card>? ca cb)])))
+
+(define (part-A L)
+  (define hands (map string->hand (string-split L "\n")))
+  (for/sum ([h (reverse (sort hands hand>?))]
+             [n (in-naturals)])
+    (* (hand-bid h) (add1 n))))
+
+(part-A test)
+(part-A data)
+
+;;
+
+
+;(part-B test)
+;(part-B data)
