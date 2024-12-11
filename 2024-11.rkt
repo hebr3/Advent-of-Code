45d940c2e9930d4e90f250e1d83b76ce55c17439
hebr3 <brook057@cougars.csusm.edu>
2024-12-11 13:01:12 -0800

    Used better logic for part 2. Need to refactor code

--- 2024-11.rkt
+++ 2024-11.rkt
@@ -26,20 +26,20 @@
 (define (mult-2024 x)
   (* x 2024))
 
-(define (blink x cnt)
+(define (blink x)
   (cond
-    [(hash-has-key? lookup-HT (list x cnt)) (hash-ref lookup-HT (list x cnt))]
+    [(hash-has-key? lookup-HT x) (hash-ref lookup-HT x)]
     [(zero? x)
      (let ([next (zero->one x)])
-       (hash-set! lookup-HT (list x cnt) next)
+       (hash-set! lookup-HT x next)
        next)]
     [(even-digits? x)
      (let ([next (break-number x)])
-       (hash-set! lookup-HT (list x cnt) next)
+       (hash-set! lookup-HT x next)
        next)]
     [else 
      (let ([next (mult-2024 x)])
-       (hash-set! lookup-HT (list x cnt) next)
+       (hash-set! lookup-HT x next)
        next)]))
 
 (define (part-A L)
@@ -48,23 +48,52 @@
     ;(println L)
     (cond
       [(zero? cnt) L]
-      [else (iter (flatten (map (λ (x) (blink x (- 25 cnt))) L)) (sub1 cnt))]))
+      [else (iter (flatten (map (λ (x) (blink x)) L)) (sub1 cnt))]))
   (length (iter INIT-STONES 25)))
 
 
-(part-A test)
-(part-A data)
+(part-A test) ;55312
+(part-A data) ;197357
 
 ;;
 
-(define (part-B L)
-  (define INIT-STONES (map string->number (string-split L)))
-  (define (iter L cnt)
-    (println (list (- 75 cnt) (length L)))
-    (cond
-      [(zero? cnt) L]
-      [else (iter (flatten (map (λ (x) (blink x (- 75 cnt))) L)) (sub1 cnt))]))
-  (length (iter INIT-STONES 75)))
+(struct stone [val cnt] #:transparent)
 
-;(part-B test)
-;(part-B data)
+(define (blink-2 val)
+  ;(println val)
+  (match-let ([(stone x cnt) val]) 
+    (cond
+      [(zero? x) (zero->one x)]
+      [(even-digits? x) (break-number x)]
+      [else (mult-2024 x)])))
+
+(define (combine-stones L HT)
+  L)
+
+(define (part-B L)
+  (define initial-HT (for/hash ([i (map string->number (string-split L))]) (values i 1)))
+  (define N 75)
+  (define (iter last-HT cnt)
+    (define next-HT (make-hash))
+    (define (update-HT s c)
+      (hash-set! next-HT s (+ c (hash-ref next-HT s 0))))
+    (cond
+      [(zero? cnt) last-HT]
+      [else 
+       ;(println cnt)
+       (for ([stone-count (hash->list last-HT)])
+         (match-let ([(cons stne cnt) stone-count])
+           (cond
+             [(zero? stne)
+              (update-HT 1 cnt)]
+             [(even-digits? stne)
+              (match-let ([(list a b) (break-number stne)])
+                (update-HT a cnt)
+                (update-HT b cnt))]
+             [else
+              (update-HT (* stne 2024) cnt)])))
+       (iter next-HT (sub1 cnt))]))
+  (for/sum ([i (hash->list (iter initial-HT N))]) (cdr i)))
+
+(part-B test)
+(part-B data)
