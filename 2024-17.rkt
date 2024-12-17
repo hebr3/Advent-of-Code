7ea9727c4a994e69d2449005ef4b09b3ea009ca0
henry brooks <brook057@cougars.csusm.edu>
2024-12-17 08:55:13 -0800

    Removed set! calls

--- 2024-17.rkt
+++ 2024-17.rkt
@@ -53,42 +53,35 @@ Program: 4,0
   (define numbers (for/list ([line lines]) (map string->number (regexp-match* #px"\\d+" line))))
   (match-define (list (list A) (list B) (list C) Program) numbers)
 
-  (define output '())
-
   (define (iter a b c pt o)
     (define (lookup v)
       (match v
         [0 0][1 1][2 2][3 3]
-        [4 A][5 B][6 C][7 (error "Shouldn't happen")]))
-    (when (< pt (length Program))
-      (define op (list-ref Program pt))
-      (define combo (list-ref Program (add1 pt)))
-      (match op
-        [0 (set! A (floor (/ A (expt 2 (lookup combo)))))]
-        [1 (set! B (bitwise-xor B combo))]
-        [2 (set! B (modulo (lookup combo) 8))]
-        [3 (iter a b c (if (zero? A) (+ 2 pt) combo) o)]
-        [4 (set! B (bitwise-xor B C))]
-        [5 (set! output (flatten (list output (modulo (lookup combo) 8))))]
-        [6 (set! B (floor (/ A (expt 2 (lookup combo)))))]
-        [7 (set! C (floor (/ A (expt 2 (lookup combo)))))])
-      (iter a b c (+ 2 pt) o)))
-  (iter A B C 0 '())
-  
-  (for ([o output])
-    (display (format "~a," o)))
-  
-  (display " : ")
-  (displayln (list A B C)))
+        [4 a][5 b][6 c][7 (error "Shouldn't happen")]))
+    (cond
+      [(< pt (length Program))
+       (define op (list-ref Program pt))
+       (define combo (list-ref Program (add1 pt)))
+       (define pt* (+ 2 pt))
+       (match op
+         [0 (iter (floor (/ a (expt 2 (lookup combo)))) b c pt* o)]
+         [1 (iter a (bitwise-xor b combo) c pt* o)]
+         [2 (iter a (modulo (lookup combo) 8) c pt* o)]
+         [3 (iter a b c (if (zero? a) pt* combo) o)]
+         [4 (iter a (bitwise-xor b c) c pt* o)]
+         [5 (iter a b c pt* (flatten (list o (modulo (lookup combo) 8))))]
+         [6 (iter a (floor (/ a (expt 2 (lookup combo)))) c pt* o)]
+         [7 (iter a b (floor (/ a (expt 2 (lookup combo)))) pt* o)])]
+      [else (list a b c o)]))
+  (iter A B C 0 '()))
 
-;; (part-A test0)
-;; (part-A test1)
-;; (part-A test2)
+(part-A test0)
+(part-A test1)
+(part-A test2)
 (part-A test3)
-;; (part-A test4)
-;; (part-A test5)
-;; (part-A data)
-(displayln "4,2,5,6,7,7,7,7,3,1,0")
+(part-A test4)
+(part-A test5)
+(part-A data)
 ;;
 
 (define test6 "Register A: 2024
