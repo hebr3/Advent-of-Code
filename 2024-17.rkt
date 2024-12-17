40cef8b424443a64f2ec9af1d3936d43906c7487
henry brooks <brook057@cougars.csusm.edu>
2024-12-17 08:07:31 -0800

    Slight refactor. Need to make it a pure function

--- 2024-17.rkt
+++ 2024-17.rkt
@@ -53,49 +53,30 @@ Program: 4,0
   (define numbers (for/list ([line lines]) (map string->number (regexp-match* #px"\\d+" line))))
   (match-define (list (list A) (list B) (list C) Program) numbers)
 
-  (define (lookup v)
-    (match v
-      [0 0][1 1][2 2][3 3]
-      [4 A][5 B][6 C][7 (error "Shouldn't happen")]))
+  (define output '())
 
-  (define (adv combo) ; 0
-    (set! A (floor (/ A (expt 2 (lookup combo))))))
-  (define (bxl combo) ; 1
-    (set! B (bitwise-xor B combo)))
-  (define (bst combo) ; 2
-    (set! B (modulo (lookup combo) 8)))
-  (define (jnz combo) ; 3
-    combo)
-  (define (bxc combo) ; 4
-    (set! B (bitwise-xor B C)))
-  (define (out combo) ; 5
-    (display (modulo (lookup combo) 8))
-    (display ","))
-  (define (bdv combo) ; 6
-    (set! B (floor (/ A (expt 2 (lookup combo))))))
-  (define (cdv combo) ; 7
-    (set! C (floor (/ A (expt 2 (lookup combo))))))
-
-  (define (iter pt)
+  (define (iter a b c pt o)
+    (define (lookup v)
+      (match v
+        [0 0][1 1][2 2][3 3]
+        [4 A][5 B][6 C][7 (error "Shouldn't happen")]))
     (when (< pt (length Program))
       (define op (list-ref Program pt))
       (define combo (list-ref Program (add1 pt)))
-      (cond
-        [(= op 3)
-         (cond
-           [(zero? A) (iter (+ 2 pt))]
-           [else (iter combo)])]
-        [else
-         (match op
-           [0 (adv combo)]
-           [1 (bxl combo)]
-           [2 (bst combo)]
-           [4 (bxc combo)]
-           [5 (out combo)]
-           [6 (bdv combo)]
-           [7 (cdv combo)])])
-      (iter (+ 2 pt))))
-  (iter 0)
+      (match op
+        [0 (set! A (floor (/ A (expt 2 (lookup combo)))))]
+        [1 (set! B (bitwise-xor B combo))]
+        [2 (set! B (modulo (lookup combo) 8))]
+        [3 (iter a b c (if (zero? A) (+ 2 pt) combo) o)]
+        [4 (set! B (bitwise-xor B C))]
+        [5 (set! output (flatten (list output (modulo (lookup combo) 8))))]
+        [6 (set! B (floor (/ A (expt 2 (lookup combo)))))]
+        [7 (set! C (floor (/ A (expt 2 (lookup combo)))))])
+      (iter a b c (+ 2 pt) o)))
+  (iter A B C 0 '())
+  
+  (for ([o output])
+    (display (format "~a," o)))
   
   (display " : ")
   (displayln (list A B C)))
@@ -103,11 +84,11 @@ Program: 4,0
 ;; (part-A test0)
 ;; (part-A test1)
 ;; (part-A test2)
- (part-A test3)
+(part-A test3)
 ;; (part-A test4)
 ;; (part-A test5)
 ;; (part-A data)
-
+(displayln "4,2,5,6,7,7,7,7,3,1,0")
 ;;
 
 (define test6 "Register A: 2024
@@ -122,4 +103,4 @@ Program: 0,3,5,4,3,0
 
 ;(part-B test0)
 ;(part-B test6)
-(part-B data)
+;(part-B data)
