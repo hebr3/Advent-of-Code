a41ec290006ed432b53cc483345cc3fbfa9f5aa8
henry brooks <brook057@cougars.csusm.edu>
2025-12-09 17:47:53 -0800

    Got a visualization working. I think the solution requires the shoelace method to check for edges.

--- 2025-09.rkt
+++ 2025-09.rkt
@@ -2,6 +2,7 @@
 
 (require rackunit)
 (require racket/set)
+(require racket/draw racket/gui)
 
 (define (input->data filename)
   (with-input-from-file filename
@@ -39,6 +40,43 @@
     (match-define (list x y) (string-split line ","))
     (point (string->number x) (string->number y))))
 
+(define (polygon->bitmap pts width height
+                         #:color [color "blue"]
+                         #:brush-style [style 'solid])
+  (define bmp (make-bitmap width height))
+  (define dc  (new bitmap-dc% [bitmap bmp]))
+
+  ;; Optional: clear background
+  (send dc set-brush "white" 'solid)
+  (send dc draw-rectangle 0 0 width height)
+
+  ;; Create a path
+  (define path (new dc-path%))
+
+  ;; Move to first point
+  (define first-pt (first pts))
+  (send path move-to (point-x first-pt) (point-y first-pt))
+
+  ;; Draw lines to remaining points
+  (for ([pt (in-list (rest pts))])
+    (send path line-to (point-x pt) (point-y pt)))
+
+  ;; Close polygon
+  (send path close)
+
+  ;; Fill the polygon
+  (send dc set-brush color style)
+  (send dc draw-path path)
+
+  bmp)
+
+(define (scale-down points)
+  (define max-x (apply max (map point-x points)))
+  (define max-y (apply max (map point-y points)))
+  (for/list ([p points])
+    (match-define (point x y) p)
+    (point (* 400 (/ x max-x)) (* 400 (/ y max-y)))))
+
 ;; Main Function
 (define (part-A input)
   (define points (parse-input input))
@@ -53,8 +91,12 @@
 ;;
 
 (define (part-B input)
-  input)
+  (define points* (parse-input input))
+  (define points (scale-down points*))
+  (define xs (map point-x points))
+  (define ys (map point-y points))
+  (polygon->bitmap points 400 400))
 
-;(check-equal? (part-B test) 0)
+(check-equal? (part-B test) 0)
 
-;(part-B data)
+(part-B data)
